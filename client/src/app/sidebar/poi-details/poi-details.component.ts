23125237de1a177c0d8cd4457a0e8e36f75c31b2
Hauke Stieler <mail@hauke-stieler.de>
2021-11-04 00:23:55 +0100

    Use opening_hours library

--- client/src/app/sidebar/poi-details/poi-details.component.ts
+++ client/src/app/sidebar/poi-details/poi-details.component.ts
@@ -3,6 +3,7 @@ import {PoiService} from "../../map/poi.service";
 import {Unsubscriber} from "../../common/ubsunscriber";
 import {Point} from "ol/geom";
 import {Feature} from "ol";
+import opening_hours, {mode, optional_conf_param} from "opening_hours";
 
 @Component({
   selector: 'app-poi-details',
@@ -11,13 +12,18 @@ import {Feature} from "ol";
 })
 export class PoiDetailsComponent extends Unsubscriber {
   name: string;
-  openingHours: string;
+  openingHoursString: string;
+  isNowOpen: boolean;
 
   constructor(private poiService: PoiService) {
     super();
     this.unsubscribeLater(poiService.poiSelected.subscribe((feature: Feature<Point>) => {
       this.name = feature.get("name");
-      this.openingHours = feature.get("opening_hours");
+      this.openingHoursString = feature.get("opening_hours");
+
+      // TODO use locale of country the POI is in
+      let openingHours = new opening_hours(this.openingHoursString, null, {locale: "de"} as optional_conf_param);
+      this.isNowOpen = openingHours.getState();
     }));
   }
 }
