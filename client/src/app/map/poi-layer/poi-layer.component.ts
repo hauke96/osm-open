6a894586c63dc30f6f712e5ca43d704d967351be
Hauke Stieler <mail@hauke-stieler.de>
2021-11-04 16:56:05 +0100

    Add feature style

--- client/src/app/map/poi-layer/poi-layer.component.ts
+++ client/src/app/map/poi-layer/poi-layer.component.ts
@@ -2,10 +2,13 @@ import {Component, OnInit} from '@angular/core';
 import {LayerService} from "../layer.service";
 import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
-import {Point} from "ol/geom";
+import {Geometry, Point} from "ol/geom";
 import {PoiService} from "../poi.service";
 import {Select} from "ol/interaction";
 import {SelectEvent} from "ol/interaction/Select";
+import {Feature} from "ol";
+import {Circle, Fill, Stroke, Style} from "ol/style";
+import RenderFeature from "ol/render/Feature";
 
 @Component({
   selector: 'app-poi-layer',
@@ -16,10 +19,13 @@ export class PoiLayerComponent implements OnInit {
   private layer: VectorLayer<VectorSource<Point>>;
   private source: VectorSource<Point>;
 
+  private red = 'rgba(244,67,54,0.65)';
+
   constructor(private layerService: LayerService, private poiService: PoiService) {
     this.source = new VectorSource<Point>();
     this.layer = new VectorLayer<VectorSource<Point>>({
-      source: this.source
+      source: this.source,
+      style: (feature) => this.getStyle(feature)
     })
 
     this.poiService.dataChanged.subscribe(newFeatures => {
@@ -28,7 +34,7 @@ export class PoiLayerComponent implements OnInit {
     });
 
     let select = new Select();
-    select.on('select', (event:SelectEvent) => {
+    select.on('select', (event: SelectEvent) => {
       this.poiService.selectPoi(event.selected[0]);
     });
     this.layerService.addInteraction(select);
@@ -36,5 +42,24 @@ export class PoiLayerComponent implements OnInit {
 
   ngOnInit(): void {
     this.layerService.addLayer(this.layer);
+  }
+
+  private getStyle(feature: RenderFeature | Feature<Geometry>) {
+    var fill = new Fill({
+      color: this.red
+    });
+    var stroke = new Stroke({
+      color: '#e0e0e0',
+      width: 1
+    });
+    return new Style({
+      image: new Circle({
+        fill: fill,
+        stroke: stroke,
+        radius: 6
+      }),
+      fill: fill,
+      stroke: stroke
+    });
   }
 }
