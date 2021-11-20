3b5a4057931a49209f96cbadbe5067089d7b3bd7
Hauke Stieler <mail@hauke-stieler.de>
2021-11-20 21:58:32 +0100

    Show table with tags

--- client/src/app/sidebar/tag-list/tag-list.component.ts
+++ client/src/app/sidebar/tag-list/tag-list.component.ts
@@ -9,7 +9,47 @@ import { Feature } from 'ol';
 })
 export class TagListComponent {
   @Input()
-  selectedFeature: Feature<Geometry>;
+  selectedFeature: Feature<Geometry> | undefined;
+
+  listExpanded: boolean = false;
+
+  private relevantKeys = [
+    'addr:street',
+    'addr:housenumber',
+    'addr:postcode',
+    'contact:phone',
+    'contact:website',
+    'contact:facebook',
+    'name',
+    'website',
+    'phone',
+    'wheelchair',
+    'wheelchair:description',
+    'toilets',
+    'toilets:wheelchair',
+    'changing_table',
+    'opening_hours',
+  ];
 
   constructor() {}
+
+  get relevantTags(): [string, string][] {
+    if (!this.selectedFeature) {
+      return [];
+    }
+
+    return Object.entries(this.selectedFeature.getProperties()).filter(t => this.relevantKeys.includes(t[0]));
+  }
+
+  isUrl(value: string): boolean {
+    let url;
+
+    try {
+      url = new URL(value);
+    } catch (_) {
+      return false;
+    }
+
+    return url.protocol === 'http:' || url.protocol === 'https:';
+  }
 }
