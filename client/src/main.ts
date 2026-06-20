1a795ff61ce3c4b7c9563066ad48996d780fce1a
Hauke Stieler <mail@hauke-stieler.de>
2026-06-20 13:28:50 +0200

    Update to Angular 21

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { appConfig } from './app/app.config';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
@@ -8,4 +8,7 @@ if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideZoneChangeDetection(), ...appConfig.providers],
+}).catch(err => console.error(err));
